1eaee3c011f02d9b93c822af4b20ae46b4f7db29
コモディ <nobu54459@gmail.com>
2024-06-06 11:22:12 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/keyV.css">
+  <link rel="stylesheet" href="./css/freelanceSummit.css">
+  <link rel="stylesheet" href="./css/EventSummary.css">
+  <link rel="stylesheet" href="./css/contents.css">
+  <link rel="stylesheet" href="./css/pickup.css">
+  <link rel="stylesheet" href="./css/timeSchedule.css">
+  <link rel="stylesheet" href="./css/faq.css">
+  <link rel="stylesheet" href="./css/footer.css">
+</head>
+
+<body>
+  <section class="key_visual">
+      <h1>
+        <img src="./img/stocksun_logo.png" alt="#">
+      </h1>
+      <div class="wrapper">
+        <p class="sub_title">日本最大規模のフリーランスイベント</p>
+        <img class="title" src="./img/kv.svg" alt="#"><br>
+        <img class="key_visual_date" src="./img/key_visual_date.png" alt="#">
+        <p class="key_visual_plase">東京国際フォーラム &nbsp;ホールE2</p>
+        <div class="key_visual_line">
+          <a class="key_visual_line_button" href="#">
+            <p>事前申し込みはこちら(無料)</p>
+            <img src="./img/LINE_Brand_icon 1.png" alt="#">
+          </a>
+        </div>
+      </div>
+  </section>
+
+  <section class="key_visual_mobile">
+    <h1>
+      <img src="./img/mobile_logo.png" alt="#">
+    </h1>
+    <p class="sub_title">日本最大規模のフリーランスイベント</p>
+    <img class="title" src="./img/mobile_title.png" alt="#"><br>
+    <img class="sponsored" src="./img/mobile_sponsored.png" alt="#"><br>
+    <img class="key_visual_date" src="./img/mobile_date.png" alt="#">
+    <p class="key_visual_plase">東京国際フォーラム &nbsp;ホールE2</p>
+  </section>
+  
+  <div class="freelance_polygon">
+    <section class="freelance_summit">
+        <h2>Freelance Summit</h2>
+        <p class="sub_title">フリーランスサミットとは？</p>
+        <p class="what_freelance_summit">「FREELANCE SUMMIT」は、StockSun株式会社が主催する<br>日本最大規模のフリーランスイベントです。</p>
+        <p class="freelance_summit_overview">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</p>
+    </section>
+
+    <section class="event_summary">
+      <h2>Event Summary</h2>
+      <p class="sub_title">イベント開催概要</p>
+      <div class="table">
+        <div class="table_flex">
+          <div class="table_date">
+            <p>開催日程</p>
+          </div>
+          <div class="table_date_contents">
+            <p>2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</p>
+          </div>
+        </div>
+        <div class="table_flex">
+          <div class="table_place">
+            <p>会場</p>
+          </div>
+          <div class="table_place_contents">
+            <p>東京国際フォーラム ホールE2</p>
+          </div>
+        </div>
+        <div class="table_flex">
+          <div class="table_deteal">
+            <p>イベント内容</p>
+          </div>
+          <div class="table_deteal_contents">
+            <p>企業ブース出展：60ブース<br>ビジネス系セミナー：12枠</p>
+          </div>
+        </div>
+        <div class="table_flex">
+          <div class="table_terget">
+            <p>対象者</p>
+          </div>
+          <div class="table_terget_contents">
+            <p>経営者 / フリーランス / 独立志望の会社員</p>
+          </div>
+        </div>
+        <div class="table_flex">
+          <div class="table_access">
+            <p>アクセス</p>
+          </div>
+          <div class="table_access_contents">
+            <p>【最寄駅】</p>
+            <ul>
+              <li>
+                <span>
+                  JR<br>
+                  有楽町駅より徒歩1分<br>東京駅より徒歩5分
+                </span>
+              </li>
+              <li>
+                <span>
+                  地下鉄<br>
+                  有楽町線 ： 有楽町駅(B1F地下コンコースにて連絡）<br>
+                  千代田線 ： 二重橋前駅より徒歩5分/日比谷駅より徒歩7分<br>
+                  丸ノ内線 ： 銀座駅より徒歩5分<br>
+                  銀座線 ： 銀座駅より徒歩7分/京橋駅より徒歩7分
+                  <br>
+                  三田線 ： 日比谷駅より徒歩5分
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div class="pc_size">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.01034651108!2d139.76117827629125!3d35.67674723021949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfabc68fde1%3A0xc4ca6a002ded263d!2z5p2x5Lqs5Zu96Zqb44OV44Kp44O844Op44Og!5e0!3m2!1sja!2sjp!4v1717606547863!5m2!1sja!2sjp" style="width: 380px; height: 235px;border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="mobile_size">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.01034651108!2d139.76117827629125!3d35.67674723021949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfabc68fde1%3A0xc4ca6a002ded263d!2z5p2x5Lqs5Zu96Zqb44OV44Kp44O844Op44Og!5e0!3m2!1sja!2sjp!4v1717606547863!5m2!1sja!2sjp" style="width: 345px; height: 200px;border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <section class="contents">
+    <div class="wrapper">
+      <h2>Contents</h2>
+      <p class="sub_title">イベント内容</p>
+      <div class="contents_flex">
+        <div class="contents_overview">
+          <p class="contents_overview_title">フリーランスに特化した企業エリア</p>
+          <div class="bg_white">
+            <p>出会いたい企業にきっと出会える</p>
+            <img src="./img/contents_line.svg" alt="#">
+            <p class="contents_overview_detail">
+              <span>60</span>
+              ブースの企業エリア
+            </p>
+            <img src="./img/contents-1.png" alt="#">
+          </div>
+        </div>
+        <div class="contents_overview">
+          <p class="contents_overview_title">各業界の著名人が登壇</p>
+          <div class="bg_white">
+            <p>ここでしか聞けない話が聞ける</p>
+            <img src="./img/contents_line.svg" alt="#">
+            <p class="contents_overview_detail">
+              豪華セミナーを開催
+            </p>
+            <img src="./img/contents-2.png" alt="#">
+          </div>
+        </div>
+      </div>
+        <div class="contents_line">
+          <a class="contents_line_button" href="#">
+            <p>事前申し込みはこちら(無料)</p>
+            <img src="./img/LINE_Brand_icon 1.png" alt="#">
+          </a>
+        </div>
+    </div>
+  </section>
+
+  <section class="pick_up">
+    <h2>Pick Up</h2>
+    <p class="sub_title">注目の企業</p>
+    <table>
+      <tr>
+        <th><img src="img/pickup_Logo-1.png" alt="#"></th>
+        <td><p>オープラン株式会社は「ITサービス」を軸に「人」と「グローバル」に向き合い日本を代表する企業になるべく2015年に産声をあげたベンチャー企業です。</p><a href="https://oplan.co.jp/">https://oplan.co.jp/</a></td>
+      </tr>
+      <tr>
+        <th><img src="img/pickup_Logo-2.png" alt="#"></th>
+        <td><p>ITフリーランスの仕事探しなら【PE-BANK】へ。Web・SE系を中心に毎月1,000件以上のご紹介。</p></td>
+      </tr>
+      <tr>
+        <th><img src="img/pickup_Logo-3.png" alt="#"></th>
+        <td><p>台湾の「食文化」の導入により、日本の飲食業をクリエイティブに。</p></td>
+      </tr>
+      <tr>
+        <th><img src="img/pickup_Logo-4.png" alt="#"></th>
+        <td><p>月額制システム開発チームレンタルSucSak(サクサク)を運営している東京新宿のシステム開発会社です。</p></td>
+      </tr>
+      <tr>
+        <th><img src="img/pickup_Logo-5.png" alt="#"></th>
+        <td><p>1000業種以上の圧倒的運用実績！リスティング、Facebook・TwitterなどのSNS広告もおまかせ。</p></td>
+      </tr>
+    </table>
+  </section>
+
+  <section class="about_exhibitors">
+    <h2>About Exhibitors</h2>
+    <p>出展企業一覧</p>
+    <ul class="company_list">
+      <li>
+        オープラン株式会社<br>
+        株式会社ビズリンク<br>
+        株式会社カルテットコミュニケーションズ<br>
+        クリーヴァ株式会社<br>
+        株式会社グラントホープ<br>
+        Modis株式会社<br>
+        INTLOOP株式会社<br>
+        Langdemy株式会社<br>
+        BOCジャパン合同会社
+      </li>
+      <li class="company_list_right">
+        株式会社アユザック<br>
+        株式会社HandAid<br>
+        株式会社J-Proach<br>
+        ラーメン協会<br>
+        株式会社おウチdeお肉<br>
+        株式会社LifeCoach<br>
+        株式会社スポーツ＆ライフイノベーション<br>
+        株式会社iGO<br>
+        株式会社エドモンド
+      </li>
+    </ul>
+    <p class="cautionary_note">（順不同、一部企業のみ掲載）</p>
+  </section>
+
+  <section class="time_schedule">
+    <h2>Time Schedule</h2>
+    <p class="sub_title">タイムスケジュール</p>
+    <div class="guest_list">
+      <div class="guest_ditail">
+        <img src="./img/ribbon.svg" class="ribbon" alt="#">
+        <p class="time">10:00〜</p>
+        <div>
+          <img src="./img/guest-1.png" alt="#">
+        </div>
+        <p>今、SNSを始めるなら</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>11:00〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-2.png" alt="#">
+        </div>
+        <p>会社員で出世する人と独立して成功する人の適正について</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>11:40〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-3.png" alt="#">
+        </div>
+        <p>営業出身のフリーランスの生存戦略</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>12:20〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-4.png" alt="#">
+        </div>
+        <p>無数のフリーランスに発注してわかった優秀な人の特徴</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>13:00〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-5.png" alt="#">
+        </div>
+        <p>現役上場企業社長が語る 最新WEBマーケティング論</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>13:40〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-6.png" alt="#">
+        </div>
+        <p>月収300万円までのロードマップ</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>14:20〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-7.png" alt="#">
+        </div>
+        <p>SNSを伸ばした先に見える世界</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>15:00〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-8.png" alt="#">
+        </div>
+        <p>元サイバーエージェント局長による広告業界の未来予想図</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>15:40〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-9.png" alt="#">
+        </div>
+        <p>エンジニアとして独立し、年収3000万円を超える方法</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>16:20〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-10.png" alt="#">
+        </div>
+        <p>累計300人以上、起業家を見て感じた成功法則</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>17:00〜</p>
+        </div>
+        <div>
+          <img src="./img/guest-11.png" alt="#">
+        </div>
+        <p>フリーランスにおすすめのM＆Aトレンドを紹介</p>
+      </div>
+      <div class="guest_ditail">
+        <div class="ribbon">
+          <p>MC</p>
+        </div>
+        <div>
+          <img src="./img/guest-12.png" alt="#">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="faq">
+    <h2>FAQ</h2>
+    <p class="sub_title">よくある質問</p>
+    <dl>
+      <dt>
+        <p class="faq_q">Q</p>
+        <p class="faq_q_contents">再入場はできますか？</p>
+      </dt>
+      <dd>
+        <p class="faq_a">A</p>
+        <p class="faq_a_contents">できます。</p>
+      </dd>
+      <dt>
+        <p class="faq_q">Q</p>
+        <p class="faq_q_contents">入場にお金はかかりますか？</p>
+      </dt>
+      <dd>
+        <p class="faq_a">A</p>
+        <p class="faq_a_contents">一切かかりません。</p>
+      </dd>
+      <dt>
+        <p class="faq_q">Q</p>
+        <p class="faq_q_contents">出展を希望する企業はどこから問い合わせれば良いですか？</p>
+      </dt>
+      <dd>
+        <p class="faq_a">A</p>
+        <p class="faq_a_contents"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdphU7Vu9xdSpRV0xaVSt9C6MpHUYG3OsTh7Vuqj8sGyInYqA/viewform">こちら</a>からお願いします。</p>
+      </dd>
+      <!-- <dt><p>入場にお金はかかりますか？</p></dt>
+      <dd><p>一切かかりません。</p></dd>
+      <dt><p>出展を希望する企業はどこから問い合わせれば良いですか？</p></dt>
+      <dd><p><a href="https://docs.google.com/forms/d/e/1FAIpQLSdphU7Vu9xdSpRV0xaVSt9C6MpHUYG3OsTh7Vuqj8sGyInYqA/viewform">こちら</a>からお願いします。</p></dd> -->
+    </dl>
+  </section>
+
+  <section class="footer">
+      <button>出展希望企業はこちら</button>
+      <!-- <hr class="footer_hr"> -->
+      <div class="footer_flex">
+        <p class="footer_flex_left">運営会社</p>
+        <img src="./img/footer_logo.png" alt="#">
+        <p class="footer_flex_right">個人情報保護方針</p>
+      </div>
+  </section>
+</body>
+</html>
