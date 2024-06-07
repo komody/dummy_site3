3c1605e8e871bb8259809ff85b80e4f8d3f3557e
コモディ <nobu54459@gmail.com>
2024-06-07 17:34:51 +0900

    feature/Read_more

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.read_more_button').click(function() {
+    $('.read_more').addClass('hidden');
+    $('.guest_list').removeClass('hidden');
+    $('.guest_ditail').removeClass('hidden');
+  });
+});
